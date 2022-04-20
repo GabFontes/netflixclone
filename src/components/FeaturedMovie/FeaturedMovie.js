79235cc8ed6d10f483fa88a6a8e8e02ea2ce3418
gab <gabrielfontes321@gmail.com>
2022-04-20 01:45:43 -0300

    feat: changing page icon and title

--- src/components/FeaturedMovie/FeaturedMovie.js
+++ src/components/FeaturedMovie/FeaturedMovie.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './FeaturedMovie.css';
 
 export const FeaturedMovie = ({ item }) => {
+  console.log(item);
   const firstDate = new Date(item.first_air_date);
   const genres = [];
   item.genres.forEach((gen) => genres.push(gen.name))
