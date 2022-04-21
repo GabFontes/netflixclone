bd9a20ae21e136a6ef1a34e8c25fa20916b65eaf
gab <gabrielfontes321@gmail.com>
2022-04-21 15:50:14 -0300

    feat: finishing movies Provider

--- src/context/MoviesProvider.js
+++ src/context/MoviesProvider.js
@@ -1,3 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import moviesContext from './MoviesContext';
 
+
+function MoviesProvider({ children }) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [enableInput, setEnableInput] = useState(false);
+  const contextValue = {
+    searchTerm,
+    setSearchTerm,
+    enableInput,
+    setEnableInput
+  }
+
+  return (
+    <moviesContext.Provider value={contextValue}>
+      {children}
+    </moviesContext.Provider>
+  )
+}
+
+export default MoviesProvider;
