fbcfe5d2ed0f97587c87718d28039c1098c2c120
gab <gabrielfontes321@gmail.com>
2022-04-21 16:29:21 -0300

    feat: fixing with_network filter in getMovieInfo fetch

--- src/apis/Tmdb.js
+++ src/apis/Tmdb.js
@@ -44,7 +44,7 @@ export const getHomeList = async () => {
     {
       slug: 'originals',
       title: 'Originais da Netflix',
-      items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+      items: await basicFetch(`/discover/tv?with_networks=213&language=pt-BR&api_key=${API_KEY}`)
     },
     {
       slug: 'trending',
